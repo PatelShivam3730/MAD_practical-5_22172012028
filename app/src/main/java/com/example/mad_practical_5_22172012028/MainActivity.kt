bc6b349b4d8c8729e26df1f38e69b3f82ee5593e
student <student@gnu.ac.in>
2023-08-18 12:26:36 +0530

    5th

--- app/src/main/java/com/example/mad_practical_5_22172012028/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_5_22172012028/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.CallLog
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 import java.net.URL
@@ -28,18 +31,18 @@ class MainActivity : AppCompatActivity() {
     }
     fun callLog()
     {
-
+        Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also { startActivity(it) }
     }
     fun gallery(gallery:String)
     {
-
+    Intent(Intent.ACTION_VIEW).setType("imgae/*").also { startActivity(it) }
     }
     fun camera()
     {
-
+    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
     }
     fun alarm()
     {
-
+    Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
     }
 }
